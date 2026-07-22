09c3bed6b7a55fa1bd76d7caaca902e1338eb733
lucas <lucaspbicalho@yahoo.com.br>
2026-07-22 14:55:28 -0300

    push

--- src/pages/ApadrinhePage.tsx
+++ src/pages/ApadrinhePage.tsx
@@ -50,6 +50,7 @@ export function ApadrinhePage() {
                   </span>
                 )}
 
+
                 {plan.value > 0 && (
                    <h3 className="text-2xl font-bold text-brand-blue">
                     {plan.title}
